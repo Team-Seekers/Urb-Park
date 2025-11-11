a3fde1ff748a8837bd433c77cdf24a3dd1b59ea1
dhivyapraban <dhivyaprabanrk@gmail.com>
2025-11-11 16:42:36 +0530

    Deleted pythonFiles/.ipynb_checkpoints directory

--- pythonFiles/.ipynb_checkpoints/yolo_inference.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-model=YOLO('yolov8n')
-
-result=model.predict("WIN_20250605_11_24_32_Pro.jpg")
-
-for box in result[0].boxes:
-    print(box.cls)
-    print(box.xyxy)
